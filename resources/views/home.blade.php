815abfd01762242d75989dc15ce0aa869d73b6f9
HELIKAS\Nexxa <nexxa9@outlook.com>
2021-03-05 20:39:08 -0500

    adding basic colors/nav bar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('app')
+
+@section('content')
+    <div class="float-container">
+        <div class="main">
+            <p>Candy gummies dessert brownie powder oat cake caramels marshmallow. Toffee dragée candy canes gummies marshmallow macaroon. Cake lemon drops cake tootsie roll gingerbread jujubes bonbon cake. Chupa chups croissant caramels. Jelly lollipop cheesecake. Cheesecake liquorice gingerbread toffee pie. Chupa chups lollipop jelly-o jelly-o chupa chups chocolate cake. Cheesecake chupa chups ice cream halvah donut apple pie sweet roll jelly beans. Caramels bonbon jelly-o. Chocolate bar sweet roll sweet roll carrot cake tart chupa chups candy. Jelly-o tart jelly beans oat cake. Cheesecake tiramisu powder.</p>
+
+            <p>Carrot cake jujubes sweet roll sweet roll jujubes jelly-o sesame snaps. Pie tart dessert topping marshmallow powder sweet dragée cookie. Danish candy candy pie sesame snaps jelly-o. Cheesecake candy cake fruitcake pie cake sweet roll chocolate cake. Carrot cake pudding pie jelly beans gingerbread danish wafer. Chocolate cake dessert biscuit ice cream. Cotton candy toffee soufflé. Carrot cake cookie lollipop fruitcake. Candy canes sweet roll candy canes marzipan topping tootsie roll tootsie roll fruitcake apple pie. Cotton candy candy canes icing marzipan jelly-o. Bonbon muffin marzipan bear claw gingerbread candy gummies marshmallow gummi bears. Sweet cotton candy jelly chocolate. Cheesecake jelly beans sweet roll chocolate cotton candy.</p>
+
+            <p>Cake dragée ice cream candy canes sweet oat cake fruitcake. Cookie carrot cake sesame snaps caramels pie bonbon gummies. Dragée lemon drops tootsie roll liquorice cotton candy muffin ice cream donut. Pastry lemon drops cupcake lemon drops jujubes dessert sweet cookie. Bonbon gummi bears carrot cake sugar plum chupa chups topping biscuit. Wafer cheesecake pie pastry sugar plum gummies tootsie roll chocolate cake. Marshmallow chocolate caramels pastry tootsie roll sesame snaps jelly-o bonbon tootsie roll. Gingerbread tootsie roll sesame snaps dessert. Pie cotton candy gummies tootsie roll lemon drops cake. Halvah cupcake chocolate oat cake dragée ice cream cupcake. Pie bonbon sugar plum tootsie roll jelly-o cheesecake ice cream caramels. Tootsie roll sugar plum sweet.</p>
+    
+        </div>
+    </div>
+
+@endsection
