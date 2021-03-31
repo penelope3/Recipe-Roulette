179b25e3dc5207d5baf3094f3fcfa1f95a0b410d
HELIKAS\Nexxa <nexxa9@outlook.com>
2021-03-31 12:19:35 -0400

    Refactor connection string to config var

--- config/database.php
+++ config/database.php
@@ -37,17 +37,8 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'Recipe_Routlette'),
-            'username' => env('DB_USERNAME', 'admin'),
-            'password' => env('DB_PASSWORD', 'test123'),
-            'options' => [
-                // here you can pass more settings to the Mongo Driver Manager
-                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
-
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
-            ],
+            'dsn' => getenv('MONGODB_URI'),
+            'database' => env('DB_DATABASE', 'Recipe_Roulette'),
         ],
 
         'sqlite' => [
