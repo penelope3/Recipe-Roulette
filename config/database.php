f2a789768a4159955147e5980c2db17cfc526fe8
HELIKAS\Nexxa <nexxa9@outlook.com>
2021-03-31 13:38:44 -0400

    db conn

--- config/database.php
+++ config/database.php
@@ -41,18 +41,6 @@ return [
             'database' => 'Recipe_Roulette',
         ],
 
-        'mongodb' => [
-            'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'reciperoutlette'),
-            'options' => [
-                // here you can pass more settings to the Mongo Driver Manager
-                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
-
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
-            ],
-        ],
         /*'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_DSN'),
