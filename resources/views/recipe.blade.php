f7bdb5bfc07f4b02f19de1682e7509be0f369348
CHarkins8 <cassandralharkins@gmail.com>
2021-04-11 20:38:48 -0400

    Recipe Page updated with new placeholder text, and possible functions are provided underneath when Recipe table in DTB is complete

--- resources/views/recipe.blade.php
+++ resources/views/recipe.blade.php
@@ -4,11 +4,47 @@
 @section('content')
     <div class="float-container">
         <div class="main">
+            <!--
             <span>Recipe Page: Stuff soon
             </span>
         </br>
             <label for="food1">mock recipe</label>
             <img src="{{ asset('images/recipe.PNG') }}" id="mock_recipe">
+            -->
+
+
+            <div>
+                <p>Like//Save</p>
+            </div>
+            <br>
+            <div>
+                <p><b>Title</b></p>
+                <p>Image<p>
+            </div>
+            <div>
+                <p>Ingredients</p>
+            </div>
+            <div>
+                <p>Instructions</p>
+            </div>
+
+
+
+
+            <?php
+                $query = { state: 'OK' };
+                $n = db.Recipe.count(query);
+                $r = Math.floor(Math.random() * n);
+                $randomElement = db.Recipe.find(query).limit(1).skip(r)->_id;
+            ?>
+            <?php
+                $recipe = raw(function($collection){ return $collection->aggregate([ ['$sample' => ['size' => 5]] ]); })->where('Active','=',true);
+            ?>
+
+
+
+
+
         </div>
     </div>
 @endsection
