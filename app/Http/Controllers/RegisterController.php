1418b4e6892652e1ffeaac8820bd737d434aa333
HELIKAS\Nexxa <nexxa9@outlook.com>
2021-04-12 20:29:59 -0400

    Slight Fixes

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
             ]);
         } catch(\Exception $e) {
             $user->delete();
-            return redirect()->route('register')->with(['error' => "We apologize, but there was an issue registering your account. " .
+            return redirect()->route('register')->with(['error' => "We apologize, but there was an issue registering your account. "
                 /**** COMMENTED OUT BECAUSE DASHBOARD ISSUES ***/ 
                 //"Please try again or try submitting a ticket on the <a href=" . route('dashboard') . " style='color: blue;'>support page!</a>
                 ]);
